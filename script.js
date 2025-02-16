e0f5c9c2a35beea0b8314c7cf40a1bd834729f01
RainOfStone <RainOfStone>
2025-02-15 19:52:24 -0500

    tested obj being passed to other funtions

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log('hello world')
+function Test() {
+    let TestObj = 'hello'
+    return {TestObj,} //can be accessed by other functions
+    Test2()
+}
+function Test2() {
+    console.log(TestObj)
+}
+Test()
